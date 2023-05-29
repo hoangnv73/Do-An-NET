58079536be9041d76499e2df860fe3dca3d8ef29
hoangnv <nguyenvanhoang73qb@gmail.com>
2023-05-29 14:37:31 +0700

    edit client

---
+++ MyWeb2023/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWeb2023.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
