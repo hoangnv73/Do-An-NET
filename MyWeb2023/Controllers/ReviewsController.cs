c2dec7de7ce9f5d31b92b158599a0e7aa5f882df
hoangnv <nguyenvanhoang73qb@gmail.com>
2023-06-14 20:52:14 +0700

    review

---
+++ MyWeb2023/Controllers/ReviewsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWeb2023.Areas.Admin.Models.Dto;
+using MyWeb2023.Areas.Admin.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyWeb2023.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReviewsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var review = await _context.Reviews.ToListAsync();
+            var result = review.Select(x => new ReviewDto
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Rating = x.Rating,
+                Comment = x.Comment,
+                PostDate = x.PostDate,
+            }).ToList();
+            return View(result);
+        }
+    }
+}
