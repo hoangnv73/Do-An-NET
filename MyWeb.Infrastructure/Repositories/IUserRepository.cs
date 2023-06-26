f2c00e3192cfa56b133e06ca021139f21db6361b
hoangnv <nguyenvanhoang73qb@gmail.com>
2023-06-26 15:20:24 +0700

    structure

---
+++ MyWeb.Infrastructure/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWeb.Infrastructure.Repositories
+{
+	public interface IUserRepository
+	{
+		Task<bool> AddAsync(string ma);
+		Task<string> GetList(string ma);
+	}
+}
---
+++ MyWeb.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWeb.Infrastructure.Repositories
+{
+	public class UserRepository : IUserRepository
+	{
+		//appcontext
+		public Task<bool> AddAsync(string ma)
+		{
+
+			//1dsldks
+			throw new NotImplementedException();
+		}
+
+		public Task<string> GetList(string ma)
+		{
+			
+			throw new NotImplementedException();
+		}
+	}
+}
