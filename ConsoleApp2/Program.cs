f291da0a739a787fbdec1d25d0df080eec889507
hoangnv <nguyenvanhoang73qb@gmail.com>
2023-06-26 15:18:37 +0700

    consolog

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+bool b = Convert.ToBoolean(5);
+Console.WriteLine("tttt: " + b);
+
+var numbers2 = new List<int>() { 1,2,3,4,5 };//1000
+foreach (int i in numbers2)
+{
+    Console.WriteLine("Day la: " + i);
+}
+// numbers[0]----> 1
+// numbers[1]----> 2
+// numbers[2]----> 3
