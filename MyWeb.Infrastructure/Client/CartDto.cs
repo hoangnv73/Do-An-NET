1259c2f2ea686c8e9e99bc5ab2fd75f67904427d
hoangnv <nguyenvanhoang73qb@gmail.com>
2023-07-04 14:39:26 +0700

    cart

---
+++ MyWeb.Infrastructure/Client/CartDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWeb.Infrastructure.Client
+{
+    public class CartDto
+    {
+        public int Total { get; set; }
+        public List<ProductCartDto> ProductCartDtos { get; set; }
+
+    }
+    public class ProductCartDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+
+    }
+}
---
+++ MyWeb2023/Controllers/CartController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWeb.Infrastructure.Client;
+using MyWeb2023.Areas.Admin.Models;
+
+namespace MyWeb2023.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CartController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(List<int> productIds)
+        {
+            
+            var products = _context.Products.Where(x => productIds.Contains(x.Id)).ToList();
+            return View();
+        }
+    }
+}
